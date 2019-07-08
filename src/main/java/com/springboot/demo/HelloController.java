70a4b8146b01231a53a37661629eb2c2ee0bde15
yangchao <yangchaonupt@163.com>
2019-07-08 21:50:39 +0800

    增加信息

--- src/main/java/com/springboot/demo/HelloController.java
+++ src/main/java/com/springboot/demo/HelloController.java
@@ -3,6 +3,10 @@ package com.springboot.demo;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 测试springboot
+ * @author yangchao
+ */
 @RestController
 public class HelloController {
 
